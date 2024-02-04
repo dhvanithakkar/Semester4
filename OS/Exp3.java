baa577229ab12ee7fb8bd2cc9bbe4cbf209822d9
Dhvani Thakkar <113112823+dhvanithakkar@users.noreply.github.com>
2024-02-04 22:21:19 +0530

    Update Exp3.java
    Better output with name of user buying the ticket

--- OS/Exp3.java
+++ OS/Exp3.java
@@ -1,75 +1,50 @@
-class BusTicketReservationSystem 
-{
-    private int seatsLeft;
+class BusReservationSystem {
+    private static int availableSeats = 1;
 
-    public BusTicketReservationSystem(int seatsLeft) 
-    {
-        this.seatsLeft = seatsLeft;
-    }
-    
-    // Synchronized
-    // public synchronized void bookSeat() throws InterruptedException 
-    // {
-    //     if (seatsLeft > 0) 
-    //     {
-    //         Thread.sleep(1000);
-    //         seatsLeft--;
-    //         System.out.println("Seat booked. Remaining seats: " + seatsLeft);
-    //     } 
-    //     else 
-    //     {
-    //         System.out.println("No seats available.");
+    // // unsynchronized, inconsistent
+    // public void reserveSeat(String passengerName) {
+    //     if (availableSeats > 0) {
+    //         System.out.println(passengerName + " booked a seat.");
+    //         availableSeats--;
+    //         System.out.println("Available seats: " + availableSeats);
+    //     } else {
+    //         System.out.println(passengerName + " couldn't book a seat. No seats available.");
     //     }
     // }
 
-    // Not synchronized
-    public void bookSeat() throws InterruptedException 
-    {
-            if (seatsLeft > 0) 
-            {
-                Thread.sleep(1000);
-                seatsLeft--;
-                System.out.println("Seat booked. Remaining seats: " + seatsLeft);
-            } 
-            else 
-            {
-                System.out.println("No seats available.");
-            }
+    // synchronized, consistent
+    public synchronized void reserveSeat(String passengerName) {
+        if (availableSeats > 0) {
+            System.out.println(passengerName + " booked a seat.");
+            availableSeats--;
+            System.out.println("Available seats: " + availableSeats);
+        } else {
+            System.out.println(passengerName + " couldn't book a seat. No seats available.");
         }
-
+    }
 }
 
-class UserThread extends Thread 
-{
-    private BusTicketReservationSystem reservationSystem;
+class PassengerThread extends Thread {
+    private BusReservationSystem reservationSystem;
+    private String passengerName;
 
-    public UserThread(BusTicketReservationSystem reservationSystem, String name) 
-    {
-        super(name);
+    public PassengerThread(BusReservationSystem reservationSystem, String passengerName) {
         this.reservationSystem = reservationSystem;
+        this.passengerName = passengerName;
     }
 
-    public void run() 
-    {
-        try 
-        {
-            reservationSystem.bookSeat();
-        } 
-        catch (InterruptedException e) 
-        {
-            e.printStackTrace();
-        }
+    public void run() {
+        reservationSystem.reserveSeat(passengerName);
     }
 }
 
-public class ps1 
-{
-    public static void main(String[] args) 
-    {
-        BusTicketReservationSystem reservationSystem = new BusTicketReservationSystem(1);
+public class BusReservation {
+    public static void main(String[] args) {
+        System.err.println("Synchronized:");
+        BusReservationSystem reservationSystem = new BusReservationSystem();
 
-        UserThread user1 = new UserThread(reservationSystem, "User 1");
-        UserThread user2 = new UserThread(reservationSystem, "User 2");
+        PassengerThread user1 = new PassengerThread(reservationSystem, "User1");
+        PassengerThread user2 = new PassengerThread(reservationSystem, "User2");
 
         user1.start();
         user2.start();
